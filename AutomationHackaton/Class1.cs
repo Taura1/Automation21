4b2b0849561fef8e282e453e3eae5cebef65e7ff
Taura <taura.frezdorfaite@devbridge.com>
2021-12-03 16:42:36 +0200

    everything

--- AutomationHackaton/Class1.cs
+++ AutomationHackaton/Class1.cs
@@ -3,7 +3,6 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
@@ -19,7 +18,10 @@ namespace AutomationHackaton
         [SetUp]
         public void Setup()
         {
-            ChromeDriver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("incognito");
+            ChromeDriver = new ChromeDriver(options);
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             ChromeDriver.Manage().Window.Maximize();
         }
 
@@ -30,29 +32,62 @@ namespace AutomationHackaton
             ChromeDriver.Dispose();
         }
 
-        [Test]
-        public void Test1()
+        public IWebElement getTableElement(string tableCoordinates)
         {
-            ChromeDriver.Navigate().GoToUrl("http://www.google.com");
+            var tableElementSelector = ChromeDriver.FindElement(By.CssSelector($"[data-square='{tableCoordinates}']"));
 
-            var acceptButton = ChromeDriver.FindElement(By.XPath(".//button[.//div[normalize-space()='Sutinku']]"));
-            acceptButton.Click();
-
-            var searchInput = ChromeDriver.FindElement(By.Name("q"));
-            searchInput.SendKeys("devbridge");
-            searchInput.SendKeys(Keys.Enter);
-
-            var searchResults = ChromeDriver.FindElements(By.XPath(".//div[@class='g']"));
-
-            var searchResult = searchResults[0];
-            var link = searchResult.FindElement(By.CssSelector("a"));
-
-            Assert.True(link.GetAttribute("href").Contains("devbridge.com"));
+            return tableElementSelector;
+        }
+        public Boolean isAlertPresent()
+        {
+            try
+            {
+                ChromeDriver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
         }
 
         [Test]
-        public void Task1()
+        public void Task1CheckMate()
         {
+            ChromeDriver.Navigate().GoToUrl("https://hackathon-chess.herokuapp.com/");
+
+            while(true)
+            {
+                Actions actions = new Actions(ChromeDriver);
+
+                actions
+                    .ClickAndHold(getTableElement("e2"))
+                    .MoveToElement(getTableElement("e4"))
+                    .Release()
+                    .ClickAndHold(getTableElement("d1"))
+                    .MoveToElement(getTableElement("h5"))
+                    .Release()
+                    .ClickAndHold(getTableElement("f1"))
+                    .MoveToElement(getTableElement("c4"))
+                    .Release()
+                    .ClickAndHold(getTableElement("h5"))
+                    .MoveToElement(getTableElement("f7"))
+                    .Release()
+                    .Perform();
+
+                Thread.Sleep(1000);
+
+                if(isAlertPresent() == true)
+                {
+                    if (ChromeDriver.SwitchTo().Alert().Text.Contains("Checkmate!"))
+                    {
+                        break;
+                    }
+                }
+                ChromeDriver.Navigate().Refresh();
+            }
+
+            Thread.Sleep(5000);
         }
 
         [Test]
@@ -64,7 +99,7 @@ namespace AutomationHackaton
         public void Task3MusicChallenge()
         {
             ChromeDriver2 = new ChromeDriver();
-            ChromeDriver2.Manage().Window.Maximize();
+            // ChromeDriver2.Manage().Window.Maximize();
             var random = new Random();
             var defaultTimeout = 200;
             var pianoKeys = new String[]{ "a", "s", "d", "f", "g", "h", "j", "k", "l", "w", "e", "t", "y", "u", "o", "p" };
@@ -122,8 +157,127 @@ namespace AutomationHackaton
         }
 
         [Test]
-        public void Task5()
+        public void Task5Labyrinth()
         {
+            ChromeDriver.Navigate().GoToUrl("http://hackathon-maze.herokuapp.com/static/");
+
+            var arrowUp = OpenQA.Selenium.Keys.ArrowUp;
+            var arrowDown = OpenQA.Selenium.Keys.ArrowDown;
+            var arrowLeft = OpenQA.Selenium.Keys.ArrowLeft;
+            var arrowRight = OpenQA.Selenium.Keys.ArrowRight;
+            var bodyElement = ChromeDriver.FindElement(By.XPath(".//body"));
+
+            void timesToTheUp(int number)
+            {
+                for (int i = 0; i < (number); i++)
+                {
+                    bodyElement.SendKeys(arrowUp);
+                }
+            }
+
+            void timesToTheLeft(int number)
+            {
+                for (int i = 0; i < (number); i++)
+                {
+                    bodyElement.SendKeys(arrowLeft);
+                }
+            }
+
+            void timesToTheRight(int number)
+            {
+                for (int i = 0; i < (number); i++)
+                {
+                    bodyElement.SendKeys(arrowRight);
+                }
+            }
+
+            void timesToTheDown(int number)
+            {
+                for (int i = 0; i < (number); i++)
+                {
+                    bodyElement.SendKeys(arrowDown);
+                }
+            }
+
+            timesToTheUp(4);
+            timesToTheLeft(2);
+            timesToTheUp(2);
+            timesToTheLeft(3);
+            timesToTheUp(2);
+            timesToTheRight(3);
+            timesToTheUp(2);
+            timesToTheRight(4);
+            timesToTheUp(4);
+            timesToTheLeft(3);
+            timesToTheDown(2);
+            timesToTheLeft(7);
+            timesToTheDown(2);
+            timesToTheLeft(4);
+            timesToTheDown(2);
+            timesToTheRight(4);
+            timesToTheDown(2);
+            timesToTheLeft(4);
+            timesToTheDown(2);
+            timesToTheLeft(2);
+            timesToTheUp(2);
+            timesToTheLeft(4);
+            timesToTheUp(2);
+            timesToTheLeft(5);
+            timesToTheDown(2);
+            timesToTheLeft(1);
+            timesToTheDown(4);
+            timesToTheLeft(2);
+            timesToTheUp(9);
+            timesToTheRight(2);
+            timesToTheUp(3);
+            timesToTheLeft(2);
+            timesToTheRight(2);
+            timesToTheDown(4);
+            timesToTheLeft(2);
+            timesToTheDown(8);
+            timesToTheRight(2);
+            timesToTheUp(6);
+            timesToTheRight(6);
+            timesToTheDown(2);
+            timesToTheRight(4);
+            timesToTheDown(2);
+            timesToTheRight(2);
+            timesToTheUp(2);
+            timesToTheRight(4);
+            timesToTheUp(2);
+            timesToTheLeft(4);
+            timesToTheUp(2);
+            timesToTheRight(4);
+            timesToTheUp(2);
+            timesToTheRight(8);
+            timesToTheUp(6);
+            timesToTheLeft(8);
+            timesToTheUp(2);
+            timesToTheLeft(2);
+            timesToTheUp(2);
+            timesToTheRight(4);
+            timesToTheUp(4);
+            timesToTheLeft(2);
+            timesToTheUp(6);
+            timesToTheLeft(3);
+            timesToTheDown(1);
+            timesToTheUp(1);
+            timesToTheLeft(7);
+            timesToTheDown(2);
+            timesToTheLeft(4);
+            timesToTheDown(6);
+            timesToTheRight(2);
+            timesToTheDown(3);
+            timesToTheRight(1);
+            timesToTheDown(1);
+            timesToTheLeft(5);
+            timesToTheDown(4);
+            timesToTheLeft(2);
+            timesToTheUp(6);
+            timesToTheLeft(2);
+            timesToTheUp(11);
+
+            Thread.Sleep(5000);
         }
 
         [Test]
@@ -156,23 +310,30 @@ namespace AutomationHackaton
         public void Task7Memes()
         {
             var http = new HttpClient();
-            var token = "xoxb-2256155121-2781463424375-nbVMXkKkzxKoFUmfCWoZiiRR";
-            http.DefaultRequestHeaders.Authorization
-                = new AuthenticationHeaderValue("Bearer", token);
+            var token = "";
+            http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var uri = "https://slack.com/api/files.upload";
-            var message = new HttpRequestMessage(HttpMethod.Post, uri);
+
+            /*
+            ChromeDriver.Navigate().GoToUrl("https://imgflip.com/memegenerator");
+            var elm = ChromeDriver.WaitForAndReturn(By.XPath(".//textarea[@placeholder='Text #1']"));
+            elm.SendKeys("LETS GO FEASTING");
+            var elm2 = ChromeDriver.WaitForAndReturn(By.XPath(".//textarea[@placeholder='Text #2']"));
+            elm2.SendKeys("Like rlly. Now.");
+            var anotherElm = ChromeDriver.WaitForAndReturn(By.XPath(".//button[@class='mm-generate b but']"));
+            anotherElm.Click();
+
+            var linkElm = ChromeDriver.WaitForAndReturn(By.XPath(".//input[@class='img-code link']"));
+            var theLink = linkElm.GetAttribute("value");*/
 
 
-
-            var file = Convert.ToBase64String(File.ReadAllBytes("C:\\Users\\taura.frezdorfaite\\Pictures\\test.jpg"), Base64FormattingOptions.None);
 
             List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
             postData.Add(new KeyValuePair<string, string>("channels", "db-automation-hackathon-2021"));
-            postData.Add(new KeyValuePair<string, string>("content", file.ToString()));
+            postData.Add(new KeyValuePair<string, string>("content", "Hellou miau. Click the upper link ;)"));
             //postData.Add(new KeyValuePair<string, string>("file", file.ToString()));
-            postData.Add(new KeyValuePair<string, string>("fileType", "auto"));
             postData.Add(new KeyValuePair<string, string>("token", token));
-            postData.Add(new KeyValuePair<string, string>("initial_comment", "Miau :3"));
+            postData.Add(new KeyValuePair<string, string>("initial_comment", "https://imgflip.com/i/5whubo"));
             //postData.Add(new KeyValuePair<string, string>("content", "Miau :3"));
 
             using (var httpClient = new HttpClient())
