dd12afd353dbc8dd025d19147da2c989ceb96381
Taura1 <42140136+Taura1@users.noreply.github.com>
2021-12-03 10:06:09 +0200

    Add files via upload

---
+++ AutomationHackaton/Class1.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationHackaton
+{
+    public class Tests
+    {
+        ChromeDriver ChromeDriver { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeDriver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ChromeDriver.Quit();
+            ChromeDriver.Dispose();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            ChromeDriver.Navigate().GoToUrl("http://www.google.com");
+
+            var acceptButton = ChromeDriver.FindElement(By.XPath(".//button[.//div[normalize-space()='Sutinku']]"));
+            acceptButton.Click();
+
+            var searchInput = ChromeDriver.FindElement(By.Name("q"));
+            searchInput.SendKeys("devbridge");
+            searchInput.SendKeys(Keys.Enter);
+
+            var searchResults = ChromeDriver.FindElements(By.XPath(".//div[@class='g']"));
+
+            var searchResult = searchResults[0];
+            var link = searchResult.FindElement(By.CssSelector("a"));
+
+            Assert.True(link.GetAttribute("href").Contains("devbridge.com"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            ChromeDriver.Navigate().GoToUrl("http://www.google.com");
+
+            var acceptButton = ChromeDriver.FindElement(By.XPath(".//button[.//div[normalize-space()='Sutinku']]"));
+            acceptButton.Click();
+
+            var searchInput = ChromeDriver.FindElement(By.Name("q"));
+            searchInput.SendKeys("devbridge");
+            searchInput.SendKeys(Keys.Enter);
+
+            var searchResults = ChromeDriver.FindElements(By.XPath(".//div[@class='g']"));
+
+            var searchResult = searchResults[1];
+            var link = searchResult.FindElement(By.CssSelector("a"));
+
+            Assert.True(link.GetAttribute("href").Contains("rekvizitai"));
+        }
+    }
+}
